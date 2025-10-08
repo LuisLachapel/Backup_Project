6e92b0954841c11616e37e9c2f252627270d0d87
LuisLachapel <luislachapelcentropeguero@gmail.com>
2025-10-08 16:34:33 -0400

    Creaciín de modelo de respuestas.

---
+++ Entity/ResponseModel.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Entity
+{
+    public class ResponseModel
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public bool Success { get; set; }
+        public dynamic? Value { get; set; }
+    }
+}
---
+++ Services/Response/ResponseApiService.cs
@@ -0,0 +1,25 @@
+﻿using Entity;
+
+namespace Services.Response
+{
+    public static class ResponseApiService
+    {
+        public static ResponseModel Response(int statusCode, object? value = null, string? message = null)
+        {
+            bool success = false;
+            if(statusCode >= 200 && statusCode < 300)
+            {
+                success = true;
+            }
+
+            var response = new ResponseModel
+            {
+                StatusCode = statusCode,
+                Value = value,
+                Message = message,
+                Success = success
+            };
+            return response;
+        }
+    }
+}
