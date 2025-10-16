cfdcf78fa91d565c3ac7f1f079d6212605d06fcc
LuisLachapel <luislachapelcentropeguero@gmail.com>
2025-10-16 16:47:00 -0400

    Icono de alerta creado.

--- frontend/src/components/user/DeleteUserButton.vue
+++ frontend/src/components/user/DeleteUserButton.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import { useUserStore } from '@/stores/userStore';
 import ErrorModal from '../ErrorModal.vue';
 import DeleteIcon from '@/assets/DeleteIcon.vue';
+import AlertIcon from '@/assets/AlertIcon.vue';
 const props = defineProps({
     id: {type: Number, required: true}
 })
@@ -66,21 +67,7 @@ const deleteUser = async () => {
             @click="closeModal = false"
             class="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
           >
-            <svg
-              class="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
+           <AlertIcon class="w-3 h-3"/>
           </button>
         </div>
 
